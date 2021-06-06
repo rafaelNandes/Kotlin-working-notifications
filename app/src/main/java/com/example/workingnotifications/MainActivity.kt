c2b057ec975069fd5595a4e375207cdf28e0fa9d
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-06-05 22:57:27 -0300

    preparing notification utils

---
+++ app/src/main/java/com/example/workingnotifications/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.workingnotifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var buttonSendNotification: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSendNotification = findViewById(R.id.button_send_notification)
+        buttonSendNotification.setOnClickListener{
+
+        }
+    }
+}
